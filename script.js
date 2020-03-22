61730ed5acc8bb340e1399798f36dffdb2804807
tukor11 <55362133+tukor11@users.noreply.github.com>
2020-03-22 23:00:05 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let name = "IFENNA SYLVESTER";
+let courses = ["HTML", "CSS", "JAVASCRIPT", "PHP", "PYTHON"];
+
+console.log(name);
+console.log(courses);
+
+let x;
+for (x = 1; x <= 200; x++) {
+    x % 2 == 1 ? console.log(x) : ""
+};
